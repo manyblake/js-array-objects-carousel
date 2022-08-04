571be4d293b065531ba9d803ba14fd5ed271139a
manyblake <manyblake@gmail.com>
2022-08-04 17:41:47 +0200

    bonus 4

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,13 @@ let currentIndex = 0;
 const pagination = document.querySelector(`.carousel__pagination`);
 const buttonsElements = [];
 
+const thumbs = [
+  `http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg`,
+  `https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg`,
+  `https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c`,
+  `https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg`,
+  `https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop`,
+];
 const slides = [
   {
     url: `http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg`,
@@ -37,7 +44,10 @@ const slides = [
 ];
 
 slides.forEach((element, i) => {
-  const button = document.createElement("button");
+  const button = document.createElement("div");
+  const buttonImage = document.createElement("img");
+  buttonImage.src = thumbs[i];
+  button.appendChild(buttonImage);
   pagination.appendChild(button);
   buttonsElements.push(button);
   button.addEventListener("click", function () {
